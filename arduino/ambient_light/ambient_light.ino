01d4bf1868740695cf19547abb339fd78e8bb543
krizajb <blaz.krizaj@gmail.com>
2016-04-21 02:31:13 +0200

    Serialization reading optimization making led strip properly responsive to client changes.

--- arduino/ambient_light/ambient_light.ino
+++ arduino/ambient_light/ambient_light.ino
@@ -13,7 +13,7 @@
 #define EPROM 0
 
 // serialization
-#define SIZE 20
+#define SIZE 100
 
 // brightness fields
 int brightness;
@@ -27,9 +27,11 @@ const char Init('I');
 const char Comma(',');
 const char Share('S');
 
+/*
 char sBuffer[SIZE + 1];
 int sBufferPos = 0;
 char sData[SIZE + 1];
+*/
 
 const bool Debug(false);
 
@@ -38,21 +40,15 @@ void log(int value, bool newLine=true);
 void log(String value, bool newLine=true);
 void log(char value, bool newLine=true);
 void remove(char* input, char ch);
+int lastIndexOf(const char * string, char target);
 
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-      Serial.end();
-  if (!Serial)
-  {
-    log("Serial port closed");
-    }
-
 // open serial port - also for debugging
   Serial.begin(9600);  
   while (!Serial) {
     //; // wait for serial port to connect. Needed for native USB port only
-    log("test");
   }
   log("Init");
 
@@ -71,15 +67,13 @@ void setup() {
   isOn = true;
 
 // set initial brightness
-  //analogWrite(STRIP, brightness); 
+  analogWrite(STRIP, brightness); 
 
+/*
 // clear serialization data
-  memset(&sBuffer[0], 0, sizeof(sBuffer));
-  memset(&sData[0], 0, sizeof(sData));
-
-#if defined(HAVE_HWSERIAL0)
-  //Serial.print("Defined");
-#endif
+  memset(&sBuffer, 0, sizeof(sBuffer));
+  memset(&sData, 0, sizeof(sData));
+*/
 }
 
 // finds last target character in string
@@ -113,9 +107,11 @@ void remove(char* string, char ch)
 // sets brightness and stores value to eprom
 void setBrightness(int value) 
 {
+  //log("Setting brightness value: "+String(value));
   analogWrite(STRIP, value); 
+  delay(30);
   EEPROM.write(EPROM, value);
-  brightnessBuffer = value;
+  //brightnessBuffer = value;
 }
 
 void log(int value, bool newLine) 
@@ -148,15 +144,24 @@ void log(char value, bool newLine)
   }
 }
 
+int sBufferPos = 0; // position in read buffer
+char sBuffer[32];
+char sByte;
+
 // the loop function runs over and over again forever
 void loop() 
-{
+{ 
  // turn off the led
   if (digitalRead(TOUCH) == HIGH && isOn) 
   {
     log("Strip OFF");
+    brightnessBuffer = brightness;
     brightness = 0;
-    setBrightness(brightness);
+    analogWrite(STRIP, brightness); 
+  // notify all interested parties
+    Serial.print("f");
+    Serial.flush();
+    
     isOn = false;
     delay(500);
   } 
@@ -164,105 +169,154 @@ void loop()
   else if (digitalRead(TOUCH) == HIGH && !isOn) 
   {
     log("Strip ON");
-    setBrightness(brightnessBuffer);
+    brightness = brightnessBuffer;
+    setBrightness(brightnessBuffer);   
+
+  // notify all interested parties
+    Serial.print("n");
+    Serial.flush();
+    
     isOn = true;
     delay(500);
   }
-  /*
-  delay(1000);
-  Serial.print(Serial.available());
-  */
 }
 
 // serial port data handler
 void serialEvent()
 {
-  //Serial.print("Data avail");
-  while (Serial.available())
+// read the incoming byte
+  sByte = Serial.read();
+  //Serial.print("D"+String(sByte)+"D ");
+   
+// add to buffer
+  sBuffer[sBufferPos++] = sByte;
+
+// init serial communication
+  if( sByte == 'I')
   {
-  // reset data
-    memset(&sData[0], 0, sizeof(sData));
+    log("Init Com");
+  // notify current brightess
+    Serial.print(brightness);
+    Serial.flush();
     
-  // read the data from serial port
-    byte size = Serial.readBytes(sData, SIZE);
-    sData[size] = 0;
-    //Serial.print(size);
-    //Serial.print(sData);
-    //Serial.flush();
+    sBuffer[sBufferPos-1] = 0;
+    sBufferPos = sBufferPos-1;
 
-  // check if this is initial connection
-    sInit = strchr(sData, 'I');
-    if (NULL != sInit) {
-      log("Found I");
-
-      digitalWrite(LED13, LOW); 
-      //delay(5000);
-      digitalWrite(LED13, HIGH); 
-      
-    // notify current brightess
-    /*
-    // this isn't tested yet, but could be the reason for init issues over time
-      if (!Serial) {
-        log("Serial not opened, reopening");
-        Serial.begin(9600); 
-      }
-      */
-      Serial.print(brightness);
-      Serial.flush();
-    // remove the initial mark
-      remove(sData, 'I');
-    }
-  
-  // prepend buffer to current data
-    if (strlen(sBuffer) != 0) 
-    {
-      char sTmp[SIZE];
-      strcpy(sTmp, sData);
-
-    // reset data
-      memset(&sData[0], 0, sizeof(sData));
-    // copy from buffer
-      strcpy(sData, sBuffer);
-    // append old data
-      strcat(sData, sTmp);
-     
-    // reset the buffer
-      sBufferPos = 0;  
-      memset(&sBuffer[0], 0, sizeof(sBuffer));
-    }
-
-  // buffer the data in case it doesn't end with comma
-    if (sData[strlen(sData)-1] != ',')
-    {
-      log("Buffer the data");  
-      int index = lastIndexOf(sData, ',');
-    // no need to copy last comma
-      for(int i = index+1; i < strlen(sData); i++)
-      {
-        sBuffer[sBufferPos++] = sData[i];
-        sData[i] = 0;
-      }
-    }
-    else 
-    {
-    // reset the buffer
-      sBufferPos = 0;  
-      memset(&sBuffer[0], 0, sizeof(sBuffer));
-    }
-       
-  // read first command
-    sCommand = strtok(sData, ",");
- 
-    while (sCommand != 0)
-    {
-      brightness = atoi(sCommand);
-      log(brightness);
-      //Serial.print(brightness);
-
-      setBrightness(brightness);
-  
-    // find the next command in data string
-      sCommand = strtok(0, ",");
-    }
+    //Serial.print("Init");
+    //Serial.print(sBuffer);
   }
+// check for delimiter
+  else if ( sByte == ',' )
+  {
+    sBuffer[sBufferPos-1] = 0;
+    log("B"+String(sBuffer)+"B ", false);
+    //Serial.print("B"+String(sBuffer)+"B ");
+    brightness = atoi(sBuffer);
+    if ( brightness >= 0 && brightness <= 255 )
+    {
+      setBrightness(brightness);
+      isOn = true;
+      log("C"+String(brightness)+"C ");
+      //Serial.print("C"+String(brightness)+"C ");
+    }     
+    sBufferPos = 0;
+  }
+
+
+    /*
+
+  
+// reset data
+  memset(&sData, 0, sizeof(sData));
+  // try using some other read method this one sucks :)
+// read the data from serial port
+  byte size = Serial.readBytes(sData, sizeof(sData));
+  sData[size] = 0;
+  Serial.print(String(size)+"| ");
+
+
+
+  //Serial.print("|"+String(sData)+"| ");
+  
+
+// check if this is initial connection
+  sInit = strchr(sData, 'I');
+  if (NULL != sInit) {
+    log("Init detected");
+   
+  // notify current brightess
+    Serial.print(brightness);
+    Serial.flush();
+  // remove the initial mark
+    remove(sData, 'I');
+  }
+
+// prepend buffer to current data
+  if (strlen(sBuffer) != 0) 
+  {
+    log("Prepend buffer");
+    char sTmp[SIZE+1];
+    strcpy(sTmp, sData);
+    Serial.print("T"+String(sTmp)+"T ");
+
+  // reset data
+    memset(&sData, 0, sizeof(sData));
+  // copy from buffer
+    strcpy(sData, sBuffer);
+  // append old data
+    strcat(sData, sTmp);
+   
+  // reset the buffer
+    sBufferPos = 0;  
+    memset(&sBuffer, 0, sizeof(sBuffer));
+  }
+  
+  Serial.print("|"+String(sData)+"| ");
+
+// buffer the data in case it doesn't end with comma
+  if (sData[strlen(sData)-1] != ',')
+  {
+    sBufferPos = 0;
+    memset(&sBuffer, 0, sizeof(sBuffer));
+    
+    
+    log("Buffer the data");  
+    int index = lastIndexOf(sData, ',');
+    int len = int(strlen(sData));
+    Serial.print("I "+String(index)+ " L "+String(len)+" ");
+  // no need to copy last comma
+    for (int i = index+1; i < len; i++)
+    {
+      Serial.print("H"+String(i)+" "+String(sData[i])+" H");
+      sBuffer[sBufferPos++] = sData[i];
+      
+      //sData[i] = 0;
+    }
+
+    for (int i = index+1; i < len; i++)
+    {     
+      sData[i] = 0;
+    }
+
+  }
+  
+  Serial.print("D"+String(sData)+"D ");
+  Serial.print("B"+String(sBuffer)+"B ");
+     
+// read first command
+  sCommand = strtok(sData, ",");
+
+  while (sCommand != 0)
+  {
+    //brightness = atoi(sCommand);
+    //log(brightness);
+    Serial.print("C"+String(sCommand)+"C ");
+
+    setBrightness(atoi(sCommand));
+
+  // find the next command in data string
+    sCommand = strtok(0, ",");
+  }
+  */
 }
+
